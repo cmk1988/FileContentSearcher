30727d3af29c18847d787a7687794e3d316ad022
cmk1988 <cmk88@web.de>
2018-05-19 00:48:45 +0200

    bug solved

--- FileContentSearcher/Test.cpp
+++ FileContentSearcher/Test.cpp
@@ -5,6 +5,8 @@ int main(int argc, char** argv)
 	if (argc != 3)
 		return EXIT_FAILURE;
 	FileContentSearcher fcs(argv[1]);
-	auto a = fcs.find_files_containing(argv[2], 4);
+	int count = 0;
+	while (argv[2][count] != '\0')count++;
+	auto a = fcs.find_files_containing(argv[2], count);
 	return EXIT_SUCCESS;
 }
