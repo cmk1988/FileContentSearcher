3c13bcfbba30e0a28608e7c9e5de05f11e6a14f3
cmk1988 <cmk88@web.de>
2018-05-18 23:41:29 +0200

    class added

--- FileContentSearcher/FileContentSearcher.h
+++ FileContentSearcher/FileContentSearcher.h
@@ -7,8 +7,7 @@ class FileContentSearcher
 public:
 	FileContentSearcher(string rootpath);
 	~FileContentSearcher();
-
-
+	vector<string> find_files_containing(char* c, int count);
 
 private:
 	FileFinder* ff;
@@ -22,5 +21,18 @@ FileContentSearcher::FileContentSearcher(string rootpath)
 FileContentSearcher::~FileContentSearcher()
 {
 	free(ff);
+}
+
+vector<string> FileContentSearcher::find_files_containing(char* c, int size)
+{
+	int count = ff->scan();
+	auto v = ff->get_filepaths();
+	vector<string> found;
+	for (size_t i = 0; i < count; i++)
+	{
+		if (FileAnalyser::analyse(v.at(i), c, size))
+			found.push_back(v.at(i));
+	}
+	return found;
 }
 
--- FileContentSearcher/Test.cpp
+++ FileContentSearcher/Test.cpp
@@ -1,16 +1,8 @@
-#include "FileFinder.h"
-#include "FileAnalyser.h"
+#include "FileContentSearcher.h"
 
 int main()
 {
-	FileFinder ff("E:\\Neuer Ordner\\");
-	int count = ff.scan();
-	auto v = ff.get_filepaths();
-	vector<string> found;
-	for (size_t i = 0; i < count; i++)
-	{
-		if (FileAnalyser::analyse(v.at(i), "test", 4))
-			found.push_back(v.at(i));
-	}
+	FileContentSearcher fcs("E:\\Neuer Ordner\\");
+	auto a = fcs.find_files_containing("test",4);
 	return EXIT_SUCCESS;
 }
